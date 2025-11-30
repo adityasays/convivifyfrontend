8f8c26ae0e14da4cb8528f1eed3506714cdc9525
adityasays <aditya.shukla019r@gmail.com>
2025-12-01 03:09:55 +0530

    Svg section

--- src/pages/home/comp/scrollpathsection.jsx
+++ src/pages/home/comp/scrollpathsection.jsx
@@ -86,7 +86,7 @@ function LinePath({ scrollYProgress }) {
 
 // Styled Components
 const SectionContainer = styled.section`
-  min-height: 270vh;
+  min-height: 300vh;
   width: 100%;
   display: flex;
   flex-direction: column;
